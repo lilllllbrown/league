72d3e9ab306661c5c1dc74351701d5a326ee99c3
lilllllbrown <lilybrown@ip0af54591.int.uvm.edu>
2015-12-08 21:34:26 -0500

    commit 3

--- home.php
+++ home.php
@@ -4,7 +4,13 @@ include ("header.php");
 include ("nav.php");
 ?>
 
-<!--<img style="max-width: 40%; display: block; border: 5px groove royalblue; border-radius: 40px;" src="media/2d/vader.JPG" alt="Hand Study">-->
+<p>This project originated as our final project for CS 148: Database Design for
+    the Web at the University of Vermont. We entered our site into the 3rd Annual 
+    CS Fair on December 9th, 2015. For more information on the CS Fair, visit 
+    their <a href="https://csfair.w3.uvm.edu/2015/">website</a>. Our project 
+    surrounds the video game League of Legends. For more information on the game 
+    or to try it out, visit their <a href="http://na.leagueoflegends.com/">website</a>. 
+      </p>
 
 <?php include ("footer.php"); ?>
 
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include ("top.php");
+include ("header.php");
+include ("nav.php");
+?>
+
+<!--<img style="max-width: 40%; display: block; border: 5px groove royalblue; border-radius: 40px;" src="media/2d/vader.JPG" alt="Hand Study">-->
+
+<?php include ("footer.php"); ?>
+
+</body>
+</html>
